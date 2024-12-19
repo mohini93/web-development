bcf78e598aff5b53fc4f94ba0f93dfce4bacc433
mohini93 <141171355+mohini93@users.noreply.github.com>
2024-12-19 13:40:30 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,60 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    updateDisplay();
+}
+
+function chooseOperator(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculateResult();
+    }
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function calculateResult() {
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operator) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+
+    currentInput = result;
+    operator = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operator = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.value = currentInput;
+}
